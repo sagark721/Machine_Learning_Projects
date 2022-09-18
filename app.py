f6086da2f4aabfe92222c34f2d0a3a2235b9f2f3
sagark721 <sagark1431@gmail.com>
2022-09-19 01:10:10 +0530

    !! Jai Ganesh !!

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "Starting Machine Learning Project"
+    return "!! Jai Ganesh !! Starting Machine Learning Project"
 
 if __name__=="__main__":
     app.run(debug=True)
