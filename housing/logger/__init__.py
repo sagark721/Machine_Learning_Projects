8af8a0bfab8c59a0b366a4857dcab47e50de87ae
sagark721 <sagark1431@gmail.com>
2022-10-12 19:14:51 +0530

    Completed

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -2,7 +2,68 @@
 import logging
 from datetime import datetime 
 import os
+import pandas as pd
 
+from housing.constant import get_current_time_stamp
+
+
+LOG_DIR = "logs"
+
+def get_log_file_name():
+    return f"log_{get_current_time_stamp()}.log"
+
+LOG_FILE_NAME = get_log_file_name()
+
+os.makedirs(LOG_DIR,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+
+
+
+logging.basicConfig(filename=LOG_FILE_PATH, 
+filemode="w",
+format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
+level=logging.INFO
+) 
+
+            
+def get_log_dataframe(file_path):
+    data=[]
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+
+    log_df= pd.DataFrame(data)
+    columns=["Time stamp","Log level","Line number","File name","Function name","Message"]
+    log_df.columns=columns
+
+    log_df["log_mesaage"] = log_df['Time stamp'].astype(str)+":$"+ log_df["Message"]
+
+    return log_df[["log_message"]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" 
 
 LOG_DIR="housing_logs"
 CURRENT_TIME_STAMP= f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
@@ -17,3 +78,4 @@ format='[%(asctime)s] %(name)s - %(levelname)s -%(message)s',
 level=logging.INFO
 )
 
+ """
